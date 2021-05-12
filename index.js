1d2106dacdfdf4a48399fc98929922827426d3b1
LoayIssa <issaloay680@gmail.com>
2021-05-13 00:39:19 +0300

    solved all pulse check

--- index.js
+++ index.js
@@ -2,21 +2,43 @@ const express = require("express");
 
 const app = express();
 const port = 3000;
+app.use(express.json())
 
-const users = ["John", "Mark"];
+
+//const users = ["John", "Mark"];
+const users = [];
+
 
 const logUsers = (req,res,next) =>{
-    console.log("logUsers")
+    console.log(users)
     next()
 }
 const logMethod  = (req,res,next)=>{
-    console.log(logMethod);
+    console.log(req.method);
     next()
 }
 //app.use("/users",logMethod)
 app.use(logUsers);
 app.get("/users",logMethod, (req, res, next) => {
-  res.json(users);
+    if(users.length===0){
+        const err = new Error("No users");
+        err.status = 500;
+        next(err)
+    }
+    else {
+        res.json(users);
+
+    }
+});
+
+app.use((err,req,res,next)=>{
+ res.status(err.status);
+ res.json({
+     error:{
+         status:err.status,
+         message: err.message,
+     },
+ });
 });
 
 
