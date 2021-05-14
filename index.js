73eada3ae2484a86a5630e153063f6b0e6653dcd
LoayIssa <issaloay680@gmail.com>
2021-05-14 16:05:15 +0300

    solved q1 and q2 in practice

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const port = 3000;
 app.use(express.json())
 
 
-//const users = ["John", "Mark"];
-const users = [];
+const users = ["John", "Mark"];
+//const users = [];
 
 
 const logUsers = (req,res,next) =>{
@@ -18,8 +18,8 @@ const logMethod  = (req,res,next)=>{
     next()
 }
 //app.use("/users",logMethod)
-app.use(logUsers);
-app.get("/users",logMethod, (req, res, next) => {
+//app.use(logUsers);
+app.get("/users", (req, res, next) => {
     if(users.length===0){
         const err = new Error("No users");
         err.status = 500;
@@ -40,6 +40,26 @@ app.use((err,req,res,next)=>{
      },
  });
 });
+//q1
+
+const usersRouter = express.Router();
+usersRouter.use((req,res,next)=>{
+    res.json(users);
+    console.log(users);
+    next();
+})
+app.use("/users",usersRouter)
+
+//q2
+
+const logUser = (req ,res,next)=>{
+    console.log(req.body.name);
+}
+app.post("/users/create",(req,res,next)=>{
+    users.push(req.body.name);
+    next()
+})
+app.use("/users/create",logUser)
 
 
 app.listen(port, () => {
